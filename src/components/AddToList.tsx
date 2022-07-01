ddef941c51f97d0bb97dd23f212d3e39d0f0c0ce
Kumoooon <jkyhj0073@gmail.com>
2022-07-01 11:48:40 +0900

    second commit

---
+++ src/components/AddToList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Input} from '@mantine/core';
+
+const AddToList = () => {
+    return (<div>
+                <Input/>
+            </div>)
+}
+export default AddToList
